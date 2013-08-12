4a45574ae0575026d547aa9e7056b5dc1d033dbf
Paul Boyce <paul.boyce@goobito.com>
2013-08-12 17:46:10 +0000

    Remove postgress and add mongo SANDBOX version

--- setup-ssjs.sh
+++ setup-ssjs.sh
@@ -10,8 +10,9 @@ heroku login
 heroku create
 ssh-keygen -t rsa
 heroku keys:add
-heroku addons:add heroku-postgresql:dev
-heroku pg:promote `heroku config  | grep HEROKU_POSTGRESQL | cut -f1 -d':'`
+heroku addons:add mongolab:sandbox
+#heroku addons:add heroku-postgresql:dev
+#heroku pg:promote `heroku config  | grep HEROKU_POSTGRESQL | cut -f1 -d':'`
 heroku plugins:install git://github.com/ddollar/heroku-config.git
 
 # Set up heroku configuration variables
